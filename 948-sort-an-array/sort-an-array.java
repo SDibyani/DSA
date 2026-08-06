c800d88bba1822cc1db3fb134cf65103f0dbf50f
SDibyani <sdibyani123@gmail.com>
2026-08-07 00:03:17 +0530

    Time: 25 ms (71.83%) | Memory: 86.6 MB (27.32%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.java
@@ -0,0 +1,50 @@
+class Solution {
+    public static void printArr(int arr[]){
+        for(int i=0; i< arr.length;i++){
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+    }
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+    public static void mergeSort(int arr[],int si,int ei){
+        if(si >= ei){
+            return;
+        }
+        int mid=si+(ei-si)/2;
+        mergeSort(arr,si,mid);
+        mergeSort(arr,mid+1,ei);
+        merge(arr,si,mid,ei);
+    }
+    public static void merge(int arr[], int si,int mid,int ei){
+        int temp[]=new int[ei-si+1];
+        int i=si;
+        int j= mid+1;
+        int k =0;
+        while(i <= mid && j <= ei){
+            if(arr[i] < arr[j]){
+                temp[k]= arr[i];
+                i++;
+            }
+            else{
+                temp[k]=arr[j];
+                j++;
+            }
+            k++;
+        }
+        //left part
+        while(i <= mid){
+            temp[k++]= arr[i++];
+        }
+        //right part
+        while(j <= ei){
+            temp[k++]=arr[j++];
+        }
+        //copy temp to original array
+        for (int l = 0; l < temp.length; l++) {
+            arr[si + l] = temp[l];
+        }
+    }
+}
